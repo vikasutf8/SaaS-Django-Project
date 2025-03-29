86c5daa986470888401916336f7f3eb611b505c1
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-03-30 00:04:51 +0530

    templates courses and views

--- course/urls.py
+++ course/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.course_list,name="course_list"),
-    path("<int:course_id>/",views.course_detail, name="course_detail="),
+    path("<int:course_id>/",views.course_detail, name="course_detail"),
     
 
 ]
--- course/views.py
+++ course/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from .models import Course
 
 # Create your views here.
 @login_required
 def course_list(request):
-    return render(request,"base.html")
+    courses =Course.objects.all()
+    if request.user.is_authenticated:
+        for course in courses:
+            course.is_unlocked =request.user in course.subscribers.all()
+    else:
+        for course in courses:
+            course.is_unlocked =False
+
+    context ={
+        "courses": courses
+    }
+    return render(request,"course_list.html",context)
+
 
 @login_required
-def course_detail(request):
-    return render(request,"base.html")
+def course_detail(request, course_id):
+    course =get_object_or_404(Course, id=course_id)
+
+    if request.user not in course.subscribers.all():
+        return redirect("course_list")
+    
+    context={
+        "course":course
+    }
+
+    return render(request,"course_detial.html", context)
